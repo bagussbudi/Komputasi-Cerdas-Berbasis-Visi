7680a6e015448e627ff7cd92104cf353768ee33c
Bagus Setyo Budi <bagussbudi@Baguss-MacBook-Air.local>
2019-06-25 19:49:23 +0700

    revisi tugas5

--- Tugas 5/tugas5.m
+++ Tugas 5/tugas5.m
@@ -1,4 +1,5 @@
 A = imread('Gambar.png');
+rgbImage = imread('Gambar.png');
 A = im2double(A);
 set(gcf,'Position',get(0,'Screensize'));
 
@@ -16,16 +17,11 @@ I = 0.6*r-0.28*g-0.32*b;
 Q = 0.21*r-0.52*g+0.31*b;
 YIQ = cat(3,Y,I,Q);
 
-Y2 = 0.29900*r + 0.58700*g + 0.11400*b;
-cb = -0.16874*r - 0.33126*g + 0.50000*b;
-cr = 0.5000*r - 0.412869*g - 0.08131*b;
-YCBCR = cat(3,Y2,cb,cr);
-
-rgbImage = imread('Gambar.png');
+YCBCR = rgb2ycbcr(rgbImage);
 HSV=rgb2hsv(rgbImage);
 
 subplot(3,3,2);imshow(A);title('Citra Asli');
 subplot(3,3,4);imshow(CMY);title('RGB to CMY');
-subplot(3,3,5);imshow(YIQ);title('RGB to YIQ');
-subplot(3,3,6);imshow(YCBCR);title('RGB to YCBCR');
-subplot(3,3,8);imshow(HSV);title('RGB to HSV');
+subplot(3,3,6);imshow(YIQ);title('RGB to YIQ');
+subplot(3,3,7);imshow(YCBCR);title('RGB to YCBCR');
+subplot(3,3,9);imshow(HSV);title('RGB to HSV');
